4ffce6d938847227de7c378847b7fd1bd04bd4dd
nichoth <nichoth@gmail.com>
2019-10-07 18:48:37 -0700

    stuff

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var test = require('tape')
 var Server = require('../')
 
-test('example', function (t) {
+test('server', function (t) {
     t.plan(1)
     var server = Server()
     t.ok(server, 'server returns')
