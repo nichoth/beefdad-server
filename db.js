d369943837769d3090b288056009ab9b8d2cea17
nichoth <nichoth@gmail.com>
2019-10-12 18:04:54 -0700

    stuff

--- db.js
+++ db.js
@@ -2,7 +2,18 @@ var level = require('level')
 
 function start () {
     var db = level(process.env.DB_NAME || 'db')
-    return db
+
+    function register ({ name, password }) {
+    }
+
+    function login ({ name, password }) {
+    }
+
+    function addFile (opts) {
+        var { username, magnet, path, filename } = opts
+    }
+
+    return { login, register, close: db.close, addFile }
 }
 
 if (require.main === module) {
