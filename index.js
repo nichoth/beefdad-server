9ddec4c00b2388e309855693d91b9fd6ae699e6a
nichoth <nichoth@gmail.com>
2019-10-17 16:29:52 -0700

    test runs

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var http = require('http')
 var env = process.env
+var url = require('url')
+var router = require('ruta3')()
+var xtend = require('xtend')
+
+function login (opts) {
+    var { res } = opts
+    if (process.env.NODE_ENV === 'development') {
+        return res.end(null, {})
+    }
+}
+
+router.addRoute('/login', login)
 
 function go () {
-    var server = http.createServer(function (req, res) {
+    var server = http.createServer(function onRequest (req, res) {
+        var path = url.parse(req.url).pathname
+        var m = router.match(path)
+        m.action(xtend(m, { req, res }))
         res.end()
     })
 
@@ -20,4 +35,3 @@ if (require.main === module) {
 }
 
 module.exports = go
-
